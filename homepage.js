4d0e70eb0ab0665e235b4e8380aa4cbed6091476
choudhurysargam <53493253+choudhurysargam@users.noreply.github.com>
2019-08-19 09:51:14 -0400

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,29 @@
+function fashionClick() {
+  fashion.onended = function () {
+    window.location.href = "fashionpage.html";
+  }
+}
+
+function mentalClick() {
+  mental.onended = function () {
+    window.location.href = "mentalhealth.html";
+  }
+}
+
+function aboutClick() {
+  about.onended = function () {
+    window.location.href = "aboutus.html";
+  }
+}
+
+// function toggleMute() {
+//
+//   var video=document.getElementById("myVideo");
+//
+//   if(video.muted){
+//     video.muted = false;
+//   } else {
+//     video.muted = true;
+//   }
+//
+// }
